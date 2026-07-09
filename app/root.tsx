b605ecd003ee8d52c6cc639b23fdec3eec2508e5
rjmn02 <108502756+rjmn02@users.noreply.github.com>
2026-07-09 21:12:39 +0800

    fix: site logo

--- app/root.tsx
+++ app/root.tsx
@@ -23,11 +23,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  {
-    rel: "icon",
-    href: "/logo.png",
-    type: "image/png",
-  },
+  { rel: "icon", href: "/logo.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
